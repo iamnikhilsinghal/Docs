4e8670cce0e89a5c1dd6b9e1509396be1a17199e
iamnikhilsinghal <31390665+iamnikhilsinghal@users.noreply.github.com>
2020-03-03 18:42:30 +0530

    Add files via upload

---
+++ Swagger/Swagger.go
@@ -0,0 +1,11 @@
+Swagger Petstore: tool for designing and documnetion of APIs
+				can authorise calls
+		can define models, database fields
+		can store example how to call an API
+
+		editor.swagger.io
+		openAPI specification
+
+		Take -->chatdetails, save, notify, save as json everything from swagger editor and use your APIs,
+		also save yaml file, 
+		can also catch go code (an HTTP handler) of the code
