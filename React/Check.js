f9951a26a73019857663dda6f56e95f63a7894c7
iamnikhilsinghal <31390665+iamnikhilsinghal@users.noreply.github.com>
2020-09-18 16:41:10 +0530

    Add files via upload

---
+++ React/Check.js
@@ -0,0 +1,9 @@
+import Header from '../Include/Header';
+
+
+import React, { Component } from 'react';
+export default class Check extends Component {
+  render() {
+    return <div>Hello world</div>;
+  }
+}
